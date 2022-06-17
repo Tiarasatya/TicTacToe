a2875ec9fd0bd1c01a820240fa79d7d987b077c6
Tiarasatya <82933768+Tiarasatya@users.noreply.github.com>
2022-06-18 03:22:07 +0700

    Update board.h

--- board.h
+++ board.h
@@ -1,14 +1,24 @@
 #pragma once
 #include<iostream>
+#include<math.h>
 using namespace std;
 
-class board
+class Board
 {
-private :
-	char sizeBoard[][];
+private : 
+	char* board;
+	int size;
 public:
-	board();
-	bool isFull();
-	bool isEmpty();  
-	void resetBoard();
+	Board();
+	Board(int size);
+	~Board();
+	int getSize();
+	void placeXO(int koor, char XO);
+	bool cekWin();
+	bool isOccupied(int koor);
+	void clearBoard();
+	void printBoard();
+	char board2D(int x, int y);
 };
+
+
